c0132f2fcc5623366d5cfb032ca387806de08fef
daplho <nskirov@gmail.com>
2022-05-11 11:55:34 -0700

    * first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Classes;
+
+var account = new BankAccount("Lauren", 3621);
+Console.WriteLine(
+    $"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
